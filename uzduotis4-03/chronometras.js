b3ba7678bc2b0d2fd3074fb75219b8f047ea7242
VickyPlein <157711444+VickyPlein@users.noreply.github.com>
2024-04-05 14:58:56 +0300

    update

---
+++ uzduotis4-03/chronometras.js
@@ -0,0 +1,7 @@
+/*
+Sukurkite chronometro aplikaciją.Aplikacija turi turėti tris mygtukus: 
+Pradėti, Stabdyti ir Pradėti iš naujo.
+Papildoma užduotis
+Padarykite dar vieną mygtuką: Fiksuoti laiką kurį paspaudus chronometras nebūtų sustabdytas, 
+bet laikas būtų užfiksuotas ir išvestas į ekraną.
+*/
